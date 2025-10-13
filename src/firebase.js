53296ad370a2112cb80451803b1dcea7f395d830
Christian Lobaugh <christian.lobaugh@gmail.com>
2025-10-13 17:13:46 -0500

    added firebase.js and installed firebase npm packages

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "***REMOVED_API_KEY***",
+  authDomain: "crate-tracker-38b6e.firebaseapp.com",
+  projectId: "crate-tracker-38b6e",
+  storageBucket: "crate-tracker-38b6e.firebasestorage.app",
+  messagingSenderId: "737369389543",
+  appId: "1:737369389543:web:68bbf2447e415aa9786542"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
